f1b6ce44207c85cc290577994e2b3d22593075db
andrea-llive <andreallive@gmail.com>
2025-05-08 21:52:47 -0500

    traspaso-prefijos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { TraspasoPrefijosComponent } from './components/prefijos/traspaso-prefijos/traspaso-prefijos.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'login',pathMatch:'full'},
   { path: 'login', component: LoginComponent },
-  {path:'inicio',component:InicioComponent},
+  { path:'inicio',component:InicioComponent},
+  { path:'tras-prefijo',component:TraspasoPrefijosComponent},
   { path: 'menus', loadChildren: () => import('./components/menus/menus.module').then(x => x.MenusModule) },
   { path: 'pages', loadChildren: () => import('./components/pages/pages.module').then(x => x.PagesModule) },
-  {path:'**',component:NotFoundComponent,pathMatch:'full'}
+  { path:'**',component:NotFoundComponent,pathMatch:'full'}
 ];
 
 @NgModule({
---
+++ src/app/components/prefijos/traspaso-prefijos/traspaso-prefijos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-traspaso-prefijos',
+  standalone: true,
+  imports: [],
+  templateUrl: './traspaso-prefijos.component.html',
+  styleUrl: './traspaso-prefijos.component.css'
+})
+export class TraspasoPrefijosComponent {
+
+}
