2875604ebc944afaf9c71be207ce6159f2b7fb47
andrea-llive <andreallive@gmail.com>
2025-05-13 00:37:41 -0500

    Modulo de eliminar prefijo

--- src/app/components/menus/menus-routing.module.ts
+++ src/app/components/menus/menus-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { CodbarComponent } from './codbar/codbar.component';
 import { MenusComponent } from './menus.component';
 import { TraspasoPrefijosComponent } from './prefijos/traspaso-prefijos/traspaso-prefijos.component';
+import { BorrarPrefijoComponent } from './prefijos/borrar-prefijo/borrar-prefijo.component';
+import { TraspasoGtinComponent } from './prefijos/traspaso-gtin/traspaso-gtin.component';
 
 const routes: Routes = [
   {
     path: '', component: MenusComponent, children: [
       {path:'codbar',component:CodbarComponent},
       {path:'tras-prefijo',component:TraspasoPrefijosComponent},
+      {path:'tras-gtin',component:TraspasoGtinComponent},
+      {path:'eliminar-prefijo',component:BorrarPrefijoComponent},
       // {path:'usuarios',component:UsuariosComponent},
       // {path:'productos',component:ProductosComponent},
       // {path:'vender',component:VenderComponent},
