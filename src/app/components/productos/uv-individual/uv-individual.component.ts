d6cbf9343860add4a3ff9caa6b3bbb996d6fe1a2
cruiz777 <cristian_ruiz_777@hotmail.com>
2025-05-28 11:50:29 -0500

    Cambios producto individual 20250528 1150

--- src/app/components/productos/uv-individual/uv-individual.component.ts
+++ src/app/components/productos/uv-individual/uv-individual.component.ts
@@ -9,6 +9,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Cliente } from 'src/app/interfaces/cliente';
+import { ClienteSeleccionadoService } from 'src/app/services/cliente-seleccionado.service';
 
 @Component({
   selector: 'app-uv-individual',
@@ -31,10 +33,14 @@ import { Router } from '@angular/router';
 export class UvIndividualComponent implements OnInit {
   formUV!: FormGroup;
   formUL!: FormGroup;
+  clienteSeleccionado: Cliente | null = null;
+  constructor(private fb: FormBuilder,
+    private clienteSeleccionadoService: ClienteSeleccionadoService,
 
-  constructor(private fb: FormBuilder) {}
+  ) {}
 
   ngOnInit(): void {
+    
     this.formUV = this.fb.group({
       codigoCliente: [''],
       cliente: [''],
@@ -94,6 +100,7 @@ export class UvIndividualComponent implements OnInit {
       tipoGtin: [''],
       observacion: ['']
     });
+    this.cargarCliente();
   }
 
   grabarTodo() {
@@ -107,4 +114,18 @@ export class UvIndividualComponent implements OnInit {
   salir(): void {
     //this.router.navigate(['/pages/clientes']);
   }
+
+  cargarCliente(): void {
+  const cliente = this.clienteSeleccionadoService.obtenerClienteActual();
+  if (cliente) {
+    this.clienteSeleccionado = cliente;
+
+    this.formUV.patchValue({
+      codigoCliente: cliente.clientes_codigo || '',
+      cliente: cliente.nomcli || '',
+      ruc: cliente.ruc || '',
+    });
+  }
+}
+
 }
