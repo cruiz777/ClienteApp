253fa2c69843d86c6dd83485267f422aa69d2227
Andoresu23c <135393056+Andoresu23c@users.noreply.github.com>
2025-11-19 10:23:33 -0500

    Correccion del environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,6 @@ export const environment = {
  applicationUrl: 'http://10.10.7.11:8000/security/api',  //Cambiar por la url de clientes en el merge
   //mantenimiento contable
  maintenanceUrl: 'http://localhost:5030/maintenance/api',
- transactionUrl: 'http://localhost:5070/transaction/api',
  clientsUrl: 'http://localhost:8000/clients/api',
 
 
@@ -13,14 +12,14 @@ export const environment = {
   validationUrl:'http://181.39.162.182:8000/validations/api',
 
   reportUrl: 'http://181.39.162.182:8000/reports/api',
-  
+
   inventoryUrl: 'http://181.39.162.182:8000/inventories/api',
   transactionUrl: 'http://181.39.162.182:8000/transaction/api',
 
     invoices_sic:'http://181.39.162.182:8000/invoices-sic/api',
     rucUlr:'http://pichincha.gapsystem.net:10048/api/services/ruc/',
     cedulaUrl:'http://pichincha.gapsystem.net:10048/api/services/cedula/',
-    
+
       apiKey: 'ilGxoWJ9Arp0JdSNuO81aq9sX6ZW9gK9767867tgb',
 
 };
