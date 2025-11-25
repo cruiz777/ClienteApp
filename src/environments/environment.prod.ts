3986faac0b150f71d8982b57284514538f82b6ba
andrea-llive <andreallive@gmail.com>
2025-11-25 16:45:32 -0500

    actualizacion enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,23 +1,23 @@
 export const environment = {
   production: true,
-   securityApiUrl: 'http://181.39.162.182:8000/security/api', // URL de la API en desarrollo
- applicationUrl: 'http://181.39.162.182:8000/security/api',  //Cambiar por la url de clientes en el merge
-
- clientsUrl: 'http://181.39.162.182:8000/clients/api',
-
-  invoicesUrl: 'http://181.39.162.182:8000/invoices/api',
+  //securityApiUrl: 'http://181.39.162.182:8000/security/api', // URL de la API en desarrollo
+ //applicationUrl: 'http://181.39.162.182:8000/security/api',  //Cambiar por la url de clientes en el merge
+ //clientsUrl: 'http://181.39.162.182:8000/clients/api',
+  //invoicesUrl: 'http://181.39.162.182:8000/invoices/api',
 
   validationUrl:'http://181.39.162.182:8000/validations/api',
-
   reportUrl: 'http://181.39.162.182:8000/reports/api',
-  
   inventoryUrl: 'http://181.39.162.182:8000/inventories/api',
   transactionUrl: 'http://181.39.162.182:8000/transaction/api',
+  invoices_sic:'http://181.39.162.182:8000/invoices-sic/api',
+  rucUlr:'http://pichincha.gapsystem.net:10048/api/services/ruc/',
+  cedulaUrl:'http://pichincha.gapsystem.net:10048/api/services/cedula/',
 
-    invoices_sic:'http://181.39.162.182:8000/invoices-sic/api',
-    rucUlr:'http://pichincha.gapsystem.net:10048/api/services/ruc/',
-    cedulaUrl:'http://pichincha.gapsystem.net:10048/api/services/cedula/',
-    
+securityApiUrl: 'http://localhost:5001security/api',
+applicationUrl: 'http://localhost:5001/security/api',
+clientsUrl: 'http://localhost:5002/clients/api',
+invoicesUrl: 'http://localhost:5003/security/api',
+
       apiKey: 'ilGxoWJ9Arp0JdSNuO81aq9sX6ZW9gK9767867tgb',
 
 };
