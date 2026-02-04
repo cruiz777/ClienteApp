6e7c0aa2692ae51bee3dbda0011539062882cf9b
Andoresu23c <135393056+Andoresu23c@users.noreply.github.com>
2026-02-04 13:09:30 -0500

    Fix: Fecha en verified y gln aumentado localización en el reporte

--- src/app/components/pages/validacion/validacion-verified/validacion-verified.component.ts
+++ src/app/components/pages/validacion/validacion-verified/validacion-verified.component.ts
@@ -146,13 +146,10 @@ export class ProductoDetalleComponent implements OnInit {
     
     try {
       const date = new Date(fecha);
-      return date.toLocaleString('es-EC', {
+      return date.toLocaleDateString('es-EC', {
         year: 'numeric',
         month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
+        day: '2-digit'
       });
     } catch (error) {
       return fecha;
